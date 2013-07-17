d0f9f0d3e2a75cd7185c4832077444d1a64478e2
root <root@AY130608150347364262Z.(none)>
2013-07-17 21:29:19 +0800

    add filel_upload

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,5 +1,10 @@
 class Asset < ActiveRecord::Base
   belongs_to :story
   attr_accessible :asset
-  has_attached_file :asset, :styles => { :large => "700x700>", :small => "80x80>" }
+  has_attached_file :asset, 
+    :styles => { :small => "40x40>" },
+    :default_url => "/images/:style/missing.png",
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :dropbox_options => { :path => proc { |style| "assets/#{style}/#{id}_#{asset.original_filename}" } }
 end
